08222a3bbf1176bd0d22b1410bb09680a4bcc6d5
SeonYeong Jeon <tjsdud9113@yu.ac.kr>
2025-04-05 19:26:34 +0900

    세 번째 커밋
    MergeTD 오류 수정

--- Algorithm_Java_Lab/src/MergeTD.java
+++ Algorithm_Java_Lab/src/MergeTD.java
@@ -1,29 +1,35 @@
 public class MergeTD extends AbstractSort{
     private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi){
-        // sorted된 두 개의 list 병합 과정
-        System.arraycopy(a, 0, aux, 0, a.length);
+        // sorted된 배열의 lo ~ hi 병합 과정
+        System.arraycopy(a, lo, aux, lo, hi - lo + 1);
 
         int i = lo; int j = mid + 1;
-        for (int n = 0 ; n < a.length; n++){
-            if (j > hi) a[n] = aux[i++];
-            else if (i > mid) a[n] = aux[j++];
-            else if (less(a[i], a[j])) a[n] = aux[i++];
-            else a[n] = aux[j++];
+        for (int n = lo ; n <= hi; n++){
+            if (j > hi)                 a[n] = aux[i++];
+            else if (i > mid)           a[n] = aux[j++];
+            else if (less(aux[i], aux[j]))  a[n] = aux[i++];
+            else                        a[n] = aux[j++];
         }
     }
 
     public static void sort(Comparable[] a){
         Comparable[] aux = new Comparable[a.length];
-        sort(a, aux, 0, a.length);
+        sort(a, aux, 0, a.length - 1);
         // recursion 호출
     }
 
     private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi){
         // 절반 sort
-        if (hi > lo) return;
-        int mid = (lo + hi) / 2;
+        if (hi <= lo) return;
+        int mid = lo + (lo + hi) / 2;
         sort(a, aux, lo, mid); // mid가 hi가 되는 걸 반복
         sort(a, aux, mid + 1, hi);
         merge(a, aux, lo, mid, hi);
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"다", "나", "가"};
+        MergeTD.sort(a);
+        MergeTD.show(a);
     }
 }
