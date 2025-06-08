189abc46d6bbe6124397204b7dd40b84a005c2da
dev-seony <tjsdud9113@gmail.com>
2025-06-08 20:54:48 +0900

    9 - DC code update
    DC(using recursion) code upload
    BST, QuickSort

---
+++ Algorithm_Java_Lab/src/BST_DC.java
@@ -0,0 +1,24 @@
+public class BST_DC {
+    public int location(int[] a, int key) {
+        int mid = location(a, 0, a.length - 1, key);
+        return mid;
+    }
+
+    private int location (int[] a, int low, int high, int key) {
+        int mid;
+        if (low > high) return -1;
+        else {
+            mid = (low + high) / 2;
+            if (key == a[mid]) return mid;
+            else if (key < a[mid]) return location(a, low, mid - 1, key);
+            else return location(a, mid + 1, high, key);
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] a = {1, 2, 5, 7, 8};
+        BST_DC bst = new BST_DC();
+        int location = bst.location(a, 8);
+        System.out.print(location);
+    }
+}
---
+++ Algorithm_Java_Lab/src/QuickSort_DC.java
@@ -0,0 +1,27 @@
+public class QuickSort_DC extends AbstractSort{
+    void quicksort(Comparable[] a) {
+        quicksort(a, 0, a.length - 1);
+    }
+    private void quicksort(Comparable[] a, int low, int high) {
+        if (high > low) {
+            int pivot = partition(a, low, high);
+            quicksort(a, low, pivot - 1);
+            quicksort(a, pivot + 1, high);
+        }
+    }
+    private int partition(Comparable[] a, int low, int high) {
+        Comparable pivot = a[low];
+        int swapPoint = low;
+        for (int i = low + 1; i <= high; i++) {
+            if (a[i].compareTo(pivot) < 0) { exch(a, i, ++swapPoint); }
+        }
+        exch(a, swapPoint, low);
+        return swapPoint;
+    }
+    public static void main(String args[]) {
+        QuickSort_DC qs = new QuickSort_DC();
+        Comparable[] a = {15, 22, 13, 27, 12, 10, 20, 25};
+        qs.quicksort(a);
+        show(a);
+    }
+}
